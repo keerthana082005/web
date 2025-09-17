8a6b2428743c6b3668847acaa3712c427fd16149
keerthana082005 <231901022@rajalakshmi.edu.in>
2025-09-17 15:22:06 +0530

    Update taskModels.js

--- taskModels.js
+++ taskModels.js
@@ -4,18 +4,3 @@ const taskSchema=new mongoose.Schema({
     completed:{type:Boolean, default:false}
 });
 module.exports=mongoose.model('Task', taskSchema);
-
-
-// /routers/taskRoutes.js
-
-const express=require('express');
-const router=express.Router();
-const {
-    createTask,
-    getAllTasks,
-    getTaskById,
-} = require('../controllers/taskController');
-router.post('/tasks',createTask);
-router.get('/tasks',getAllTasks);
-router.get('/tasks/:id', getTaskById);
-module.exports=router;
